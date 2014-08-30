a9d4c2fc643ef7a5e0d18919cca977e013d029c0
Angel Medrano <amedrano@cvs.com>
2014-08-30 19:28:19 -0400

    Adds access data

--- api/src/github.com/asmedrano/wri/main.go
+++ api/src/github.com/asmedrano/wri/main.go
@@ -15,7 +15,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/lakes", LakesHandler)
-	r.HandleFunc("/geom", GeomHandler)
+	r.HandleFunc("/lakes/geom", LakesGeomHandler)
+	r.HandleFunc("/access", AccessHandler)
 	http.Handle("/", r)
 	fmt.Print("Starting Server...\n")
 	http.ListenAndServe(":8000", nil)
@@ -75,22 +76,12 @@ func queryType(name string, qtype string) map[string]string {
 		"qtype": qtype,
 	}
 	return m
-}
 
-var queryMap = map[string]map[string]string{
-	"n":  queryType("name", "ILIKE"), //name
-	"p":  queryType("pond", "="),          // pond
-	"t":  queryType("trout_stk", "="),     // trout
-	"pa": queryType("pubacc", "="),        // pubacc
-	"br": queryType("boatramp", "="),      //boatramp
-	"c":  queryType("cold", "="),          //cold
-	"i":  queryType("island", "="),
 }
-
-func parseLakesQueryStr(r *http.Request) map[string]string {
+func parseQueryStr(r *http.Request, qm map[string]map[string]string) map[string]string {
 	m := map[string]string{}
 
-	for k, _ := range queryMap {
+	for k, _ := range qm {
 		v := r.FormValue(k)
 		if v != "" {
 			m[k] = v
@@ -100,15 +91,25 @@ func parseLakesQueryStr(r *http.Request) map[string]string {
 	return m
 }
 
+var lakesQueryMap = map[string]map[string]string{
+	"n":  queryType("name", "ILIKE"),  //name
+	"p":  queryType("pond", "="),      // pond
+	"t":  queryType("trout_stk", "="), // trout
+	"pa": queryType("pubacc", "="),    // pubacc
+	"br": queryType("boatramp", "="),  //boatramp
+	"c":  queryType("cold", "="),      //cold
+	"i":  queryType("island", "="),
+}
+
 func buildLakesQuery(m map[string]string) (string, []interface{}) {
 	query := ""
 	vals := []interface{}{}
 	prefix := "WHERE"
 	for k, v := range m {
-		qm := queryMap[k]
+		qm := lakesQueryMap[k]
 		if qm["qtype"] == "ILIKE" {
-            v = "%" + v + "%"
-        }
+			v = "%" + v + "%"
+		}
 		vals = append(vals, v)
 		if len(vals)-1 > 0 {
 			prefix = "AND"
@@ -124,7 +125,7 @@ func LakesHandler(w http.ResponseWriter, r *http.Request) {
 	db := GetDB()
 	defer db.Close()
 
-	rQ := parseLakesQueryStr(r)
+	rQ := parseQueryStr(r, lakesQueryMap)
 	qs, qv := buildLakesQuery(rQ)
 	var rows *sql.Rows
 	var err error
@@ -165,52 +166,198 @@ func LakesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-    w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 
 }
 
-
 type GeomResource struct {
-    Gid int64
-    Name string
-    Geom string
+	Gid  int64
+	Name string
+	Geom string
 }
 
-func GeomHandler(w http.ResponseWriter, r *http.Request) {
-    db := GetDB()
+func LakesGeomHandler(w http.ResponseWriter, r *http.Request) {
+	db := GetDB()
 	defer db.Close()
 	var rows *sql.Rows
 	var err error
-    r.ParseForm()
-    geoms := r.Form["g"]
-    results := []GeomResource{}
-    query:= fmt.Sprintf("SELECT gid, name, ST_AsGeoJSON(geom) FROM lakes WHERE gid IN (%s)", strings.Join(geoms, ","))
-    rows, err = db.Query(query)
+	r.ParseForm()
+	geoms := r.Form["g"]
+	results := []GeomResource{}
+	query := fmt.Sprintf("SELECT gid, name, ST_AsGeoJSON(geom) FROM lakes WHERE gid IN (%s)", strings.Join(geoms, ","))
+	rows, err = db.Query(query)
 	defer rows.Close()
 	for rows.Next() {
-        g := GeomResource{}
+		g := GeomResource{}
 		if err := rows.Scan(&g.Gid, &g.Name, &g.Geom); err != nil {
 			log.Print(err)
 		}
-        results = append(results, g)
+		results = append(results, g)
 	}
 
 	if err = rows.Err(); err != nil {
 		log.Print(err)
 	}
 
-    js, err := json.Marshal(results)
+	js, err := json.Marshal(results)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-    w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 }
 
+/*
+ Access Data looks like:
+
+ gid       | integer                | not null default nextval('fish_access_gid_seq'::regclass)
+ id        | numeric                |
+ name      | character varying(50)  |
+ rmp_cns   | character varying(254) |
+ rmp_cnd   | character varying(254) |
+ park      | character varying(50)  |
+ rstrctn   | character varying(254) |
+ uni       | character varying(1)   |
+ type      | character varying(20)  |
+ wat_ttp   | character varying(8)   |
+ lat       | double precision       |
+ lon       | double precision       |
+ ownership | character varying(20)  |
+ geom      | geometry(Point)        |
+
+*/
+var accessQueryMap = map[string]map[string]string{
+	"n":  queryType("name", "ILIKE"), //name
+	"wt": queryType("wat_ttp", "="),  // water type
+}
+
+// TODO:Dry this up
+func buildAccessQuery(m map[string]string) (string, []interface{}) {
+	query := ""
+	vals := []interface{}{}
+	prefix := "WHERE"
+	for k, v := range m {
+		qm := accessQueryMap[k]
+		if qm["qtype"] == "ILIKE" {
+			v = "%" + v + "%"
+		}
+		vals = append(vals, v)
+		if len(vals)-1 > 0 {
+			prefix = "AND"
+		}
+		query += fmt.Sprintf(" %s %s %s $%d", prefix, qm["name"], qm["qtype"], len(vals))
+
+	}
+	return query, vals
+}
+
+type AccessResource struct {
+	Gid         int64 
+	Id          string
+	Name        string
+	Rmp_Cns     sql.NullString
+	Rmp_Cnd     sql.NullString
+	Park        sql.NullString
+	Restriction sql.NullString
+	Uni         sql.NullString
+	Type        sql.NullString
+	Wat_ttp     sql.NullString
+	Lat         float32
+	Lon         float32
+	Ownership   string
+	Geom        string
+}
+
+func (a *AccessResource) MarshalJSON() ([]byte, error){
+    m := struct {
+        Gid         int64
+        Id          string
+        Name        string
+        Rmp_Cns     string
+        Rmp_Cnd     string
+        Park        string
+        Restriction string
+        Uni         string
+        Type        string
+        Wat_ttp     string
+        Lat         float32
+        Lon         float32
+        Ownership   string
+        Geom        string
+    }{
+        a.Gid, 
+        a.Id,
+        a.Name, 
+        a.Rmp_Cns.String,
+        a.Rmp_Cnd.String,
+        a.Park.String,
+        a.Restriction.String,
+        a.Uni.String,
+        a.Type.String,
+        a.Wat_ttp.String,
+        a.Lat,
+        a.Lon,
+        a.Ownership, 
+        a.Geom,
+    }
+
+    return json.Marshal(m)
+}
+
+
+func AccessHandler(w http.ResponseWriter, r *http.Request) {
+	db := GetDB()
+	defer db.Close()
+
+	rQ := parseQueryStr(r, accessQueryMap)
+	qs, qv := buildAccessQuery(rQ)
+	var rows *sql.Rows
+	var err error
+	query := "SELECT gid, id, name, rmp_cns, rmp_cnd, park, rstrctn, uni, type, wat_ttp, lat, lon, ownership, ST_AsGeoJSON(geom) FROM fish_access"
+	if qs != "" {
+		query += qs
+	}
+	query += " ORDER BY name"
+
+	if qs == "" {
+		rows, err = db.Query(query)
+	} else {
+		rows, err = db.Query(query, qv...)
+	}
+	defer rows.Close()
+
+	results := []AccessResource{}
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	for rows.Next() {
+		r := AccessResource{}
+		if err := rows.Scan(&r.Gid, &r.Id, &r.Name, &r.Rmp_Cns, &r.Rmp_Cnd, &r.Park, &r.Restriction, &r.Uni, &r.Type, &r.Wat_ttp, &r.Lat, &r.Lon, &r.Ownership, &r.Geom); err != nil {
+		    log.Print(err)
+		}
+		results = append(results, r)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Print(err)
+	}
+
+	js, err := json.Marshal(results)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
 
 // A little stub to connect to our database
 func GetDB() *sql.DB {
